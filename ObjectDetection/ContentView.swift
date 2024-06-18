b2d526067b44bf05ffd94377ad8cb3c9080c078b
magistraapta <magsitraagis@gmail.com>
2024-06-18 15:49:02 +0800

    Initial Commit

---
+++ ObjectDetection/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  ObjectDetection
+//
+//  Created by Magistra Apta on 18/06/24.
+//
+
+import SwiftUI
+import AVFoundation
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ObjectDetection/ObjectDetectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObjectDetectionApp.swift
+//  ObjectDetection
+//
+//  Created by Magistra Apta on 18/06/24.
+//
+
+import SwiftUI
+
+@main
+struct ObjectDetectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
